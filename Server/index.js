990c8dc78aee1405fc1845fc2cd4b87ca0da64f1
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-10-08 08:49:22 -0400

    completed basic functionality of authentication for registering and logging in of users

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
+//Routers Import
+import AuthRoute from './Routes/AuthRoute.js';
 
+//Middleware
 const app = express();
 app.use(bodyParser.json({limit:'30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}));
@@ -12,3 +15,6 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> 
 app.listen(process.env.PORT, () => 
 console.log("Listening"))).catch((error)=>console.log(error));
+
+//useage of routes
+app.use('/auth', AuthRoute)
